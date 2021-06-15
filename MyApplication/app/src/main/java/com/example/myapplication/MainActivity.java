a625bfbdaa9e5cf20174a0ab7a5b27bf11fa7a35
yanhao <yahoo2000@126.com>
2021-06-15 20:59:04 +0800

    修bug

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,155 +1,412 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.widget.FrameLayout;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
-
-import com.ashokvarma.bottomnavigation.BottomNavigationBar;
-import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+//import android.support.v7.app.AppCompatActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
-import java.util.List;
-
-public class MainActivity extends AppCompatActivity {
-
-    private Fragment mFragment;
-    private BottomNavigationBar bottom_navigation_bar_container;
-    //点击菜单键弹出菜单窗口
-    private BottomNavigationItem menu;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private WebView webView;
-    //记录当前fragment
-    private int currentFragment = 2;
-    //记录currentFragment是否需要变更
-//    public static boolean currentFragmentFlag = false;
+    private ProgressBar progressBar;
+    private EditText textUrl;
+    private ImageView webIcon, goBack, goForward, navSet, goHome, btnStart;
+
+    private long exitTime = 0;
+
+    private Context mContext;
+    private InputMethodManager manager;
+
+    private static final String HTTP = "http://";
+    private static final String HTTPS = "https://";
+    private static final int PRESS_BACK_EXIT_GAP = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // 防止底部按钮上移
+        getWindow().setSoftInputMode
+                (WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN |
+                        WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
         setContentView(R.layout.activity_main);
-        bottom_navigation_bar_container = findViewById(R.id.buttomNavigationBar);
-        initBottomBar();
+
+        mContext = MainActivity.this;
+        manager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+
+        // 绑定控件
+        initView();
+
+        // 初始化 WebView
+        initWeb();
     }
 
-    //初始化底部导航栏
-    private void initBottomBar() {
-        bottom_navigation_bar_container.setMode(BottomNavigationBar.MODE_FIXED);
-        bottom_navigation_bar_container.setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_STATIC);
-        bottom_navigation_bar_container.setBarBackgroundColor(R.color.gray);
-        menu = new BottomNavigationItem(R.drawable.user, "");
-        bottom_navigation_bar_container
-                .addItem(new BottomNavigationItem(R.drawable.back, "")).setActiveColor(R.color.black).setInActiveColor(R.color.black)
-                .addItem(new BottomNavigationItem(R.drawable.next, "")).setActiveColor(R.color.purple_200).setInActiveColor(R.color.black)
-//                .addItem(new BottomNavigationItem(R.drawable.search,"")).setActiveColor(R.color.purple_200).setInActiveColor(R.color.black)
-                .addItem(new BottomNavigationItem(R.drawable.home, "")).setActiveColor(R.color.purple_200).setInActiveColor(R.color.black)
-                .addItem(new BottomNavigationItem(R.drawable.window, "")).setActiveColor(R.color.purple_200).setInActiveColor(R.color.black)
-                .addItem(new BottomNavigationItem(R.drawable.user, "")).setActiveColor(R.color.purple_200).setInActiveColor(R.color.black)
-                .initialise();
-        initFirstFragment();
-        bottom_navigation_bar_container.setTabSelectedListener(new BottomNavigationBar.OnTabSelectedListener() {
-            //当点击进入这个fragment
+    /**
+     * 绑定控件
+     */
+    private void initView() {
+        webView = findViewById(R.id.webView);
+        progressBar = findViewById(R.id.progressBar);
+        textUrl = findViewById(R.id.textUrl);
+        webIcon = findViewById(R.id.webIcon);
+        btnStart = findViewById(R.id.btnStart);
+        goBack = findViewById(R.id.goBack);
+        goForward = findViewById(R.id.goForward);
+        navSet = findViewById(R.id.navSet);
+        goHome = findViewById(R.id.goHome);
+
+        // 绑定按钮点击事件
+        btnStart.setOnClickListener(this);
+        goBack.setOnClickListener(this);
+        goForward.setOnClickListener(this);
+        navSet.setOnClickListener(this);
+        goHome.setOnClickListener(this);
+
+        // 地址输入栏获取与失去焦点处理
+        textUrl.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
-            public void onTabSelected(int position) {
-                Log.d("tab select now page:", "" + currentFragment);
-                Log.d("tab select dest page", "" + position);
-//                String s = ""+currentFragment;
-//                Toast.makeText(MainActivity.this,s,Toast.LENGTH_SHORT).show();
-
-                Fragment fragment = fragmentFactory.getFragment(position);
-                //创建fragment管理器
-                FragmentManager fm = getSupportFragmentManager();
-                //获取fragment事务
-                FragmentTransaction transaction = fm.beginTransaction();
-                //判断fragment是否添加
-                if (!fragment.isAdded()) { //如果没有添加,进行添加到activity
-                    transaction.add(R.id.fragment_content, fragment);
-                } else {   //添加了，就进行显示
-                    transaction.show(fragment);
-                }
-//                    transaction.show(fragment);
-                //保存并提交
-                transaction.commit();
-
-                if (position > 1) {
-                    currentFragment = position;
-                }
-
-            }
-
-            //从这个fragment离开
-            @Override
-            public void onTabUnselected(int position) {
-                if (position > 1) {
-                    Fragment fragment = fragmentFactory.getFragment(position);
-                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                    //离开tab.进行隐藏
-                    transaction.hide(fragment);
-                    transaction.commit();
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus) {
+                    // 显示当前网址链接 TODO:搜索页面显示搜索词
+                    textUrl.setText(webView.getUrl());
+                    // 光标置于末尾
+                    textUrl.setSelection(textUrl.getText().length());
+                    // 显示因特网图标
+//                    webIcon.setImageResource(R.drawable.internet);
+                    // 显示跳转按钮
+//                    btnStart.setImageResource(R.drawable.go);
                 } else {
+                    // 显示网站名
+                    textUrl.setText(webView.getTitle());
+                    // 显示网站图标
+                    webIcon.setImageBitmap(webView.getFavicon());
+                    // 显示刷新按钮
+//                    btnStart.setImageResource(R.drawable.refresh);
                 }
             }
+        });
 
+        // 监听键盘回车搜索
+        textUrl.setOnKeyListener(new View.OnKeyListener() {
             @Override
-            public void onTabReselected(int position) {
-
+            public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER && keyEvent.getAction() == KeyEvent.ACTION_DOWN) {
+                    // 执行搜索
+                    btnStart.callOnClick();
+                    textUrl.clearFocus();
+                }
+                return false;
             }
         });
     }
 
-    //初始化fragment
-    private void initFirstFragment() {
-        //使用fragmentTransaction.hide和show可能会导致重影
-        FragmentManager fragmentManager = getSupportFragmentManager(); //v4包里的类
-        List<Fragment> fragments = fragmentManager.getFragments();//获取有多少个fragment,并且添加到list集合里面
-        if (fragments != null && fragments.size() > 0) { //当list集合里面的fragment对象不为空，且fragment对象个数大于0时，执行
-            for (Fragment fragment : fragments) {  // 容器中元素类型   临时变量 ：容器变量  foreach循环会自动遍历容器中每个元素
-                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                //移除残留的fragment
-                fragmentTransaction.remove(fragment);
-                //提交并保存
-                fragmentTransaction.commit();
+
+    /**
+     * 初始化 web
+     */
+    @SuppressLint("SetJavaScriptEnabled")
+    private void initWeb() {
+        // 重写 WebViewClient
+        webView.setWebViewClient(new bWebviewclient());
+        // 重写 WebChromeClient
+        webView.setWebChromeClient(new bWebChromeClient());
+
+        WebSettings settings = webView.getSettings();
+        // 启用 js 功能
+        settings.setJavaScriptEnabled(true);
+        // 设置浏览器 UserAgent
+        settings.setUserAgentString(settings.getUserAgentString() + " mkBrowser/" + getVerName(mContext));
+
+        // 将图片调整到适合 WebView 的大小
+        settings.setUseWideViewPort(true);
+        // 缩放至屏幕的大小
+        settings.setLoadWithOverviewMode(true);
+
+        // 支持缩放，默认为true。是下面那个的前提。
+        settings.setSupportZoom(true);
+        // 设置内置的缩放控件。若为false，则该 WebView 不可缩放
+        settings.setBuiltInZoomControls(true);
+        // 隐藏原生的缩放控件
+        settings.setDisplayZoomControls(false);
+
+        // 缓存
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        // 设置可以访问文件
+        settings.setAllowFileAccess(true);
+        // 支持通过JS打开新窗口
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);
+        // 支持自动加载图片
+        settings.setLoadsImagesAutomatically(true);
+        // 设置默认编码格式
+        settings.setDefaultTextEncodingName("utf-8");
+        // 本地存储
+        settings.setDomStorageEnabled(true);
+        settings.setPluginState(WebSettings.PluginState.ON);
+
+        // 资源混合模式
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+
+        // 加载首页
+        webView.loadUrl(getResources().getString(R.string.home_url));
+    }
+
+
+    /**
+     * 重写 WebViewClient
+     */
+    private class bWebviewclient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // 设置在webView点击打开的新网页在当前界面显示,而不跳转到新的浏览器中
+
+            if (url == null) {
+                // 返回true自己处理，返回false不处理
+                return true;
+            }
+
+            // 正常的内容，打开
+            if (url.startsWith(HTTP) || url.startsWith(HTTPS)) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            // 调用第三方应用，防止crash (如果手机上没有安装处理某个scheme开头的url的APP, 会导致crash)
+            try {
+                // TODO:弹窗提示用户，允许后再调用
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+                return true;
+            } catch (Exception e) {
+                return true;
             }
         }
-        //进入时默认显示第一个fragment界面
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        Fragment fragment = fragmentFactory.getFragment(2);
-        fragmentTransaction.add(R.id.fragment_content, fragment);
-        fragmentTransaction.commit();
 
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            // 网页开始加载，显示进度条
+            progressBar.setProgress(0);
+            progressBar.setVisibility(View.VISIBLE);
+
+            // 更新状态文字
+            textUrl.setText("加载中...");
+
+            // 切换默认网页图标
+//            webIcon.setImageResource(R.drawable.internet);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            // 网页加载完毕，隐藏进度条
+            progressBar.setVisibility(View.INVISIBLE);
+
+            // 改变标题
+            setTitle(webView.getTitle());
+            // 显示页面标题
+            textUrl.setText(webView.getTitle());
+        }
+    }
+
+
+    /**
+     * 重写 WebChromeClient
+     */
+    private class bWebChromeClient extends WebChromeClient {
+        private final static int WEB_PROGRESS_MAX = 100;
+
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            super.onProgressChanged(view, newProgress);
+
+            // 加载进度变动，刷新进度条
+            progressBar.setProgress(newProgress);
+            if (newProgress > 0) {
+                if (newProgress == WEB_PROGRESS_MAX) {
+                    progressBar.setVisibility(View.INVISIBLE);
+                } else {
+                    progressBar.setVisibility(View.VISIBLE);
+                }
+            }
+        }
+
+        @Override
+        public void onReceivedIcon(WebView view, Bitmap icon) {
+            super.onReceivedIcon(view, icon);
+
+            // 改变图标
+            webIcon.setImageBitmap(icon);
+        }
+
+        @Override
+        public void onReceivedTitle(WebView view, String title) {
+            super.onReceivedTitle(view, title);
+
+            // 改变标题
+            setTitle(title);
+            // 显示页面标题
+            textUrl.setText(title);
+        }
+    }
+
+    /**
+     * 返回按钮处理
+     */
+    @Override
+    public void onBackPressed() {
+        // 能够返回则返回上一页
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            if ((System.currentTimeMillis() - exitTime) > PRESS_BACK_EXIT_GAP) {
+                // 连点两次退出程序
+                Toast.makeText(mContext, "再按一次退出程序",
+                        Toast.LENGTH_SHORT).show();
+                exitTime = System.currentTimeMillis();
+            } else {
+                super.onBackPressed();
+            }
+
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            // 跳转 或 刷新
+            case R.id.btnStart:
+                if (textUrl.hasFocus()) {
+                    // 隐藏软键盘
+                    if (manager.isActive()) {
+                        manager.hideSoftInputFromWindow(textUrl.getApplicationWindowToken(), 0);
+                    }
+
+                    // 地址栏有焦点，是跳转
+                    String input = textUrl.getText().toString();
+                    if (!isHttpUrl(input)) {
+                        // 不是网址，加载搜索引擎处理
+                        try {
+                            // URL 编码
+                            input = URLEncoder.encode(input, "utf-8");
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
+                        }
+                        input = "https://www.baidu.com/s?wd=" + input + "&ie=UTF-8";
+                    }
+                    webView.loadUrl(input);
+
+                    // 取消掉地址栏的焦点
+                    textUrl.clearFocus();
+                } else {
+                    // 地址栏没焦点，是刷新
+                    webView.reload();
+                }
+                break;
+
+            // 后退
+            case R.id.goBack:
+                webView.goBack();
+                break;
+
+            // 前进
+            case R.id.goForward:
+                webView.goForward();
+                break;
+
+            // 设置
+            case R.id.navSet:
+                Toast.makeText(mContext, "功能开发中", Toast.LENGTH_SHORT).show();
+                break;
+
+            // 主页
+            case R.id.goHome:
+                webView.loadUrl(getResources().getString(R.string.home_url));
+                break;
+
+            default:
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        try {
+            webView.getClass().getMethod("onPause").invoke(webView);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        try {
+            webView.getClass().getMethod("onResume").invoke(webView);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 判断字符串是否为URL（https://blog.csdn.net/bronna/article/details/77529145）
+     *
+     * @param urls 要勘定的字符串
+     * @return true:是URL、false:不是URL
+     */
+    public static boolean isHttpUrl(String urls) {
+        boolean isUrl;
+        // 判断是否是网址的正则表达式
+        String regex = "(((https|http)?://)?([a-z0-9]+[.])|(www.))"
+                + "\\w+[.|\\/]([a-z0-9]{0,})?[[.]([a-z0-9]{0,})]+((/[\\S&&[^,;\u4E00-\u9FA5]]+)+)?([.][a-z0-9]{0,}+|/?)";
+
+        Pattern pat = Pattern.compile(regex.trim());
+        Matcher mat = pat.matcher(urls.trim());
+        isUrl = mat.matches();
+        return isUrl;
+    }
+
+    /**
+     * 获取版本号名称
+     *
+     * @param context 上下文
+     * @return 当前版本名称
+     */
+    private static String getVerName(Context context) {
+        String verName = "unKnow";
+        try {
+            verName = context.getPackageManager().
+                    getPackageInfo(context.getPackageName(), 0).versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return verName;
     }
 }
-
-
-    //获取当前fragment,作为fragmentFactory替代品
-//    public Fragment getFragment(int position,int currentFragment){
-//        currentFragment = this.currentFragment;
-//        WebViewFragment webViewFragment = new WebViewFragment();
-//        switch (position) {
-//            case 0:
-//                webViewFragment.GoBack();
-//                return webViewFragment;
-//            case 1:
-//                webViewFragment.GoAhead();
-//                return webViewFragment;
-//            case 2:
-//                webViewFragment.LoadURL("www.baidu.com");
-//                return webViewFragment;
-////                return new FirstFragment();
-//            case 3:
-//                webViewFragment.LoadURL("www.baidu.com");
-//                return webViewFragment;
-////                return new FirstFragment();
-//            case 4:
-//                return new FirstFragment();
-//        }
-//        return new FirstFragment();
-//    }
-//}
